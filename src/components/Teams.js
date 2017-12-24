579d82304ae2d50e9bbec206d95affba5a5a4e1a
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-12-24 11:58:43 -0700

    Code for teams-first

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
 
 export default class Teams extends Component {
-  render() {
+  state = {
+    teamNames: [],
+    loading: true,
+  }
+  componentDidMount () {
+    getTeamNames()
+      .then((teamNames) => {
+        this.setState(() => ({
+          loading: false,
+          teamNames,
+        }))
+      })
+  }
+  render () {
+    const { loading, teamNames } = this.state
+    const { location, match } = this.props
+
     return (
       <div className='container two-column'>
-        TEAMS
+        <Sidebar
+          loading={loading}
+          title='Teams'
+          list={teamNames}
+          {...this.props}
+        />
+
+        {loading === false && location.pathname === '/teams'
+          ? <div className='sidebar-instruction'>Select a Team</div>
+          : null}
       </div>
     )
   }
