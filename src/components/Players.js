0912364097016ab79e20268131c48c89bb5c5f49
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-12-24 11:54:18 -0700

    Code for players-first

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import { getPlayers } from '../api'
+import { parse } from 'query-string'
+import slug from 'slug'
 
 export default class Players extends Component {
-  render() {
+  state = {
+    players: [],
+    loading: true,
+  }
+  componentDidMount () {
+    const { location } = this.props
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers()
+  }
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then((players) => this.setState(() => ({
+        loading: false,
+        players,
+      })))
+  }
+  render () {
+    const { players, loading } = this.state
+    const { match, location } = this.props
+
     return (
       <div className='container two-column'>
-        PLAYERS
+        <Sidebar
+          loading={loading}
+          title='Players'
+          list={players.map((player) => player.name)}
+          {...this.props}
+        />
+
+        {loading === false && location.pathname === '/players'
+          ? <div className='sidebar-instruction'>Select a Player</div>
+          : null}
+
       </div>
     )
   }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, Route } from 'react-router-dom'
+import slug from 'slug'
+
+Sidebar.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+}
+
+function CustomLink ({ to, children }) {
+  return (
+    <Route
+      path={to.pathname}
+      children={({ match }) => (
+        <li style={{listStyleType: 'none', fontWeight: match ? 'bold' : 'normal'}}>
+          <Link to={to}>{children}</Link>
+        </li>
+      )}
+    />
+  )
+}
+
+export default function Sidebar ({title, list, loading, location, match}) {
+  return loading === true
+    ? <h1>LOADING</h1>
+    : <div>
+        <h3 className='header'>{title}</h3>
+        <ul className='sidebar-list'>
+          {list.map((item) => (
+            <CustomLink
+              key={item}
+              to={{
+                pathname: `${match.url}/${slug(item)}`,
+                search: location.search
+              }}>
+                {item.toUpperCase()}
+            </CustomLink>
+          ))}
+        </ul>
+      </div>
+}
