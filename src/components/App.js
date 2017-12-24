f9e2b051a41970d553b62fc2dc68777b9f638c2a
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-12-24 11:29:34 -0700

    Code for catch-all commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import Navbar from './Navbar'
 import Players from './Players'
@@ -12,9 +12,12 @@ class App extends Component {
         <div>
           <Navbar />
 
-          <Route path='/' exact component={Home} />
-          <Route path='/players' component={Players} />
-          <Route path='/teams' component={Teams} />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
+          </Switch>
         </div>
       </Router>
     )
