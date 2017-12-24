931b9048b981756929d1c106d40faa1aacc35695
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-12-24 11:34:27 -0700

    Code for home commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import TeamLogo from './TeamLogo'
+import { Link } from 'react-router-dom'
+import { getTeamNames } from '../api'
 
 export default class Home extends Component {
+  state = {
+    teamNames: []
+  }
+  componentDidMount () {
+    getTeamNames()
+      .then((teamNames) => this.setState(() => ({
+        teamNames
+      })))
+  }
   render() {
+    const { teamNames } = this.state
+
     return (
       <div className='container'>
-        HOME
+        <h1 className='large-header'>
+          Hash History Basketball League
+        </h1>
+        <h3 className='header text-center'>
+          Select a team
+        </h3>
+        <div className='home-grid'>
+          {teamNames.map((id) => (
+            <Link key={id} to={`/${id}`}>
+              <TeamLogo id={id} width='125px' />
+            </Link>
+          ))}
+        </div>
       </div>
     )
   }
