c74ea8b53d05fa1bb268c4c38aa84a69abac2d59
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-12-24 17:15:40 -0700

    Changes for animate branch

--- src/components/Players.js
+++ src/components/Players.js
@@ -5,6 +5,11 @@ import { getPlayers } from '../api'
 import { parse } from 'query-string'
 import slug from 'slug'
 
+import {
+  TransitionGroup,
+  CSSTransition,
+} from 'react-transition-group'
+
 export default class Players extends Component {
   state = {
     players: [],
@@ -40,6 +45,7 @@ export default class Players extends Component {
           ? <div className='sidebar-instruction'>Select a Player</div>
           : null}
 
+
         <Route path={`${match.url}/:playerId`} render={({match}) => {
           if (loading === true) return null
 
@@ -48,29 +54,33 @@ export default class Players extends Component {
           } = players.find((player) => slug(player.name) === match.params.playerId)
 
           return (
-            <div className='panel'>
-              <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
-              <h1 className='medium-header'>{name}</h1>
-              <h3 className='header'>#{number}</h3>
-              <div className='row'>
-                <ul className='info-list' style={{marginRight: 80}}>
-                  <li>Team
-                    <div>
-                      <Link style={{color: '#68809a'}} to={`/${teamId}`}>
-                        {teamId[0].toUpperCase() + teamId.slice(1)}
-                      </Link>
-                    </div>
-                  </li>
-                  <li>Position<div>{position}</div></li>
-                  <li>PPG<div>{ppg}</div></li>
-                </ul>
-                <ul className='info-list'>
-                  <li>APG<div>{apg}</div></li>
-                  <li>SPG<div>{spg}</div></li>
-                  <li>RPG<div>{rpg}</div></li>
-                </ul>
-              </div>
-            </div>
+            <TransitionGroup className='panel'>
+              <CSSTransition key={location.key} classNames='fade' timeout={250}>
+                <div className='panel'>
+                  <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
+                  <h1 className='medium-header'>{name}</h1>
+                  <h3 className='header'>#{number}</h3>
+                  <div className='row'>
+                    <ul className='info-list' style={{marginRight: 80}}>
+                      <li>Team
+                        <div>
+                          <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                            {teamId[0].toUpperCase() + teamId.slice(1)}
+                          </Link>
+                        </div>
+                      </li>
+                      <li>Position<div>{position}</div></li>
+                      <li>PPG<div>{ppg}</div></li>
+                    </ul>
+                    <ul className='info-list'>
+                      <li>APG<div>{apg}</div></li>
+                      <li>SPG<div>{spg}</div></li>
+                      <li>RPG<div>{rpg}</div></li>
+                    </ul>
+                  </div>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
           )
         }} />
       </div>
